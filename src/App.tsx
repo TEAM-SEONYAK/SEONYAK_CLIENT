b6b3f4c705074ead38ef2ebdf3e4030af6d69595
se0jinYoon <tjwls1999@gmail.com>
2024-07-04 20:13:07 +0900

    init: 색상, 폰트, reset 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import { Global, ThemeProvider } from '@emotion/react';
+import styled from '@emotion/styled';
+
+import Main from './pages/main/Main';
+import globalStyle from './styles/globalStyle';
+import theme from './styles/theme';
+
 const App = () => {
-  return <></>;
+  return (
+    <Wrapper>
+      <ThemeProvider theme={theme}>
+        <Global styles={globalStyle} />
+        <Main />
+      </ThemeProvider>
+    </Wrapper>
+  );
 };
 
 export default App;
+
+const Wrapper = styled.div`
+  min-height: calc(var(--vh, 1vh) * 100);
+  border: none;
+
+  background-color: '#FFFFFF';
+`;
--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,5 +1,12 @@
+import styled from '@emotion/styled';
+
 const Main = () => {
-  return <div>Main</div>;
+  return <TestDiv>메인 테스트 할게요</TestDiv>;
 };
 
 export default Main;
+
+const TestDiv = styled.div`
+  background-color: ${({ theme }) => theme.colors.Red};
+  ${({ theme }) => theme.fonts.Head1_B_20};
+`;
