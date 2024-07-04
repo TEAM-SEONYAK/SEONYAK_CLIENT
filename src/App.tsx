4730509f6409395f87937e389f45ae65dc5a5e6a
se0jinYoon <tjwls1999@gmail.com>
2024-07-04 20:18:33 +0900

    feat: 모바일뷰 반응형 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
 import { Global, ThemeProvider } from '@emotion/react';
 import styled from '@emotion/styled';
+import { useEffect } from 'react';
 
 import Main from './pages/main/Main';
 import globalStyle from './styles/globalStyle';
 import theme from './styles/theme';
 
 const App = () => {
+  const setScreenSize = () => {
+    // vh 관련
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+    // window width 관련
+    const windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    const maxWidth = Math.min(375, windowWidth);
+    document.documentElement.style.setProperty('--app-max-width', `${maxWidth}px`);
+  };
+
+  useEffect(() => {
+    setScreenSize();
+    window.addEventListener('resize', setScreenSize);
+
+    return () => {
+      window.removeEventListener('resize', setScreenSize);
+    };
+  }, []);
+
   return (
     <Wrapper>
       <ThemeProvider theme={theme}>
