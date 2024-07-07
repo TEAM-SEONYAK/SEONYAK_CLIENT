45b635296ecc714aa28b4a267f0ced7e6105eb6a
lydiacho <sheecho0804@ewhain.net>
2024-07-08 00:24:13 +0900

    chore: 반영되지 않은 import문 수정

--- src/pages/onboarding/seniorOnboarding/SeniorOnboardingPage.tsx
+++ src/pages/onboarding/seniorOnboarding/SeniorOnboardingPage.tsx
@@ -1,8 +1,8 @@
 import TitleBox from '../components/TitleBox';
-import { SENIOR_STEPS } from '../constants';
+import { SENIOR_ONBOARDING_STEPS } from '../constants';
 
 const SeniorOnboardingPage = () => {
-  const { title, description } = SENIOR_STEPS[0];
+  const { title, description } = SENIOR_ONBOARDING_STEPS[0];
   return (
     <div>
       <TitleBox title={title} description={description} />
