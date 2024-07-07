792f545f542bd5abe9f452f998d81a6af8a90358
se0jinYoon <tjwls1999@gmail.com>
2024-07-08 00:13:35 +0900

    chore: 필요없는 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
 import { Global, ThemeProvider } from '@emotion/react';
 import styled from '@emotion/styled';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
-import { TempLogoIc, ArrowLeftIc, AlarmIc } from './assets/svgs';
-import { FullBtn } from './components/commons/Button';
-import { Header } from './components/commons/Header';
-import Input from './components/commons/Input';
-import ProgressBar from './components/commons/ProgressBar';
-import WarnDescription from './components/commons/WarnDescription';
 import globalStyle from './styles/globalStyle';
 import theme from './styles/theme';
 const App = () => {
@@ -22,8 +16,6 @@ const App = () => {
     document.documentElement.style.setProperty('--app-max-width', `${maxWidth}px`);
   };
 
-  const [inputVal, setInputVal] = useState('');
-
   useEffect(() => {
     setScreenSize();
     window.addEventListener('resize', setScreenSize);
@@ -33,28 +25,10 @@ const App = () => {
     };
   }, []);
 
-  const onClickTest = () => {
-    alert('hi');
-  };
-
   return (
     <Wrapper>
       <ThemeProvider theme={theme}>
         <Global styles={globalStyle} />
-        <Header LeftSvg={ArrowLeftIc} onClickLeft={onClickTest} title="개인정보 입력" />
-        <Header LeftSvg={TempLogoIc} onClickLeft={onClickTest} onClickRight={onClickTest} RightSvg={AlarmIc} />
-        <Header LeftSvg={ArrowLeftIc} onClickLeft={onClickTest} />
-        <Header title="개인정보 입력" RightSvg={AlarmIc} onClickRight={onClickTest} />
-        <ProgressBar max={10} current={1} />
-        <Input
-          placeholder="주요 경력 및 이력을 최대 60자까지 작성해주세요"
-          wordLimit={200}
-          height={29.1}
-          inputVal={inputVal}
-          setInputVal={setInputVal}
-        />
-        <WarnDescription show={inputVal.length > 200} warnText="글자 수가 초과되었어요" />
-        <FullBtn isActive={true} text="다음으로" onClick={onClickTest} />
       </ThemeProvider>
     </Wrapper>
   );
