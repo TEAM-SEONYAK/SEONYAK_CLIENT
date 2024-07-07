123c8cab27c4d26f27bc0043e05a5a80bc0e0a1e
lydiacho <sheecho0804@ewhain.net>
2024-07-07 19:22:01 +0900

    chore: 사용하지 않는 코드/파일 삭제

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,7 +2,6 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import App from './App';
 import HomePage from './pages/home/Home';
 import JuniorPromisePage from './pages/juniorPromise/JuniorPromise';
-import MyPage from './pages/myPage/MyPage';
 import JuniorOnboardingPath from './pages/onboarding/juniorOnboarding/JuniorOnboarding';
 import SeniorOnboardingPage from './pages/onboarding/seniorOnboarding/SeniorOnboarding';
 import SeniorProfilePage from './pages/seniorProfile/SeniorProfile';
