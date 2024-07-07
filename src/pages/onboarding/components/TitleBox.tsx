c83342b79475ae1f4a024799908bc604534ad3d5
lydiacho <sheecho0804@ewhain.net>
2024-07-07 21:10:00 +0900

    design: TitleBox 퍼블리싱

---
+++ src/pages/onboarding/components/TitleBox.tsx
@@ -0,0 +1,32 @@
+import styled from '@emotion/styled';
+
+interface TitleBoxProps {
+  title: string;
+  children: string;
+}
+
+const TitleBox = ({ title, children }: TitleBoxProps) => {
+  return (
+    <Wrapper>
+      <Heading1>{title}</Heading1>
+      <Description>{children}</Description>
+    </Wrapper>
+  );
+};
+
+export default TitleBox;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`;
+
+const Heading1 = styled.h1`
+  ${({ theme }) => theme.fonts.Head1_B_20};
+`;
+
+const Description = styled.p`
+  ${({ theme }) => theme.fonts.Body1_M_14};
+  color: ${({ theme }) => theme.colors.grayScaleMG2};
+`;
--- src/pages/onboarding/seniorOnboarding/SeniorOnboardingPage.tsx
+++ src/pages/onboarding/seniorOnboarding/SeniorOnboardingPage.tsx
@@ -1,5 +1,12 @@
+import TitleBox from '../components/TitleBox';
+
 const SeniorOnboardingPage = () => {
-  return <div>SeniorOnboarding</div>;
+  return (
+    <div>
+      <TitleBox title="선약을 시작해볼까요?">개인정보는 안전하게 보관되어 누구에게도 공개되지 않아요.</TitleBox>
+      <div>SeniorOnboarding</div>
+    </div>
+  );
 };
 
 export default SeniorOnboardingPage;
