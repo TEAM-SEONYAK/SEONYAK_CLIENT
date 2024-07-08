55247b52651d87258c2ade4bc8338f91d7d2985c
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-07-08 21:50:05 +0900

    fix: 서버이슈 해결

--- src/pages/onboarding/commons/apis/loginAxios.ts
+++ src/pages/onboarding/commons/apis/loginAxios.ts
@@ -4,7 +4,8 @@ export const loginAxios = (authorizationCode: string | undefined) => {
   return axios.post(
     'https://api.seonyak-dev.kro.kr/api/v1/auth/login',
     {
-      redirectUri: 'https://api.seonyak-dev.kro.kr/login/oauth2/code/google',
+      // redirectUri: 'https://api.seonyak-dev.kro.kr/login/oauth2/code/google',
+      redirectUri: 'http://localhost:5173',
       socialType: 'GOOGLE',
     },
     {
