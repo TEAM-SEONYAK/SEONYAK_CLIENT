f886edd4dc66eb64ff45bdb4083a71619ea3a2ac
lydiacho <sheecho0804@ewhain.net>
2024-07-09 03:10:05 +0900

    design: TitleBox 추가 및 step prop 추가

--- src/pages/onboarding/components/Layout.tsx
+++ src/pages/onboarding/components/Layout.tsx
@@ -1,18 +1,22 @@
 import styled from '@emotion/styled';
 import { ReactNode } from 'react';
+import TitleBox from './TitleBox';
 import { AlarmIc, TempLogoIc } from '../../../assets/svgs';
 import { FullBtn } from '../../../components/commons/FullButton';
 import { Header } from '../../../components/commons/Header';
 import ProgressBar from '../../../components/commons/ProgressBar';
+import { SENIOR_ONBOARDING_STEPS } from '../constants';
 
-const Layout = ({ children }: { children: ReactNode }) => {
+const Layout = ({ step, children }: { step: number; children: ReactNode }) => {
+  const { title, description } = SENIOR_ONBOARDING_STEPS[step - 1];
   return (
     <Wrapper>
-      <div>
-        <Header title="타이틀" LeftSvg={TempLogoIc} RightSvg={AlarmIc} />
-        <ProgressBar max={5} current={2} />
-      </div>
-      {children}
+      <Header title="타이틀" LeftSvg={TempLogoIc} RightSvg={AlarmIc} />
+      <ProgressBar max={10} current={step} />
+      <Content>
+        <TitleBox title={title} description={description} />
+        {children}
+      </Content>
       <FullBtn
         text="텍스트"
         isActive
@@ -33,4 +37,13 @@ const Wrapper = styled.section`
 
   height: 100dvh;
   padding: 5.8rem 2rem 3.6rem;
+`;
+
+const Content = styled.article`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+
+  height: 100%;
+  padding-top: 3rem;
 `;
--- src/pages/onboarding/juniorOnboarding/JuniorOnboardingPage.tsx
+++ src/pages/onboarding/juniorOnboarding/JuniorOnboardingPage.tsx
@@ -1,7 +1,14 @@
 import Layout from '../components/Layout';
 
 const JuniorOnboardingPage = () => {
-  return <Layout>JuniorOnboarding</Layout>;
+  return (
+    <Layout step={1}>
+      <>
+        <h1>테스트1</h1>
+        <h2>테스트2</h2>
+      </>
+    </Layout>
+  );
 };
 
 export default JuniorOnboardingPage;
