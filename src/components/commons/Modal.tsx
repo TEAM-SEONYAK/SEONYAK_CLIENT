514ba320d5026ea2d6425525dcaca83495418cfb
se0jinYoon <tjwls1999@gmail.com>
2024-07-08 20:07:27 +0900

    feat: 버튼 클릭해서 닫히는 모달 구현

---
+++ src/components/commons/Modal.tsx
@@ -0,0 +1,105 @@
+import styled from '@emotion/styled';
+import React, { useRef, useState } from 'react';
+import BtnModalContent from './BtnModalContent';
+import { CloseIc } from '../../assets/svgs';
+import useClickOutside from '../../hooks/useClickOutside';
+
+interface BtnCloseModalPropType {
+  title: string;
+  showModal: boolean;
+  // eslint-disable-next-line no-unused-vars
+  handleShowModal: (type: boolean) => void;
+  children?: React.ReactNode;
+  btnText: string;
+}
+
+export const BtnCloseModal = (props: BtnCloseModalPropType) => {
+  const { title, showModal, handleShowModal, children, btnText } = props;
+  const modalRef = useRef(null);
+
+  const handleModalClose = () => {
+    handleShowModal(false);
+  };
+
+  // 커스텀 훅 활성화용 state
+  const [modalBgClickActive, setModalBgClickActive] = useState(showModal);
+
+  // 커스텀 훅 전달 함수
+  const handleOutSideClick = () => {
+    if (showModal) {
+      setModalBgClickActive(true);
+    }
+    if (modalBgClickActive) {
+      handleShowModal(false);
+      setModalBgClickActive(false);
+    }
+  };
+
+  // 커스텀 훅 사용
+  useClickOutside(modalRef, handleOutSideClick);
+
+  return (
+    <ModalBackground $showModal={showModal}>
+      <BtnModalWrapper ref={modalRef}>
+        <CloseIcon onClick={handleModalClose} />
+        <BtnModalTitle>{title}</BtnModalTitle>
+        {children}
+        <BtnModalContent />
+        <BtnModalBtn onClick={handleModalClose}>{btnText}</BtnModalBtn>
+      </BtnModalWrapper>
+    </ModalBackground>
+  );
+};
+
+const ModalBackground = styled.div<{ $showModal: boolean }>`
+  display: ${({ $showModal }) => ($showModal ? 'flex' : 'none')};
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  z-index: 2;
+
+  width: 100vw;
+  height: 100%;
+
+  background-color: ${({ theme }) => theme.colors.transparentBlack_65};
+`;
+
+const BtnModalWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 1.8rem;
+  align-items: center;
+  position: relative;
+
+  width: 30rem;
+  padding: 3.6rem 1.3rem 1.5rem;
+  border: none;
+  border-radius: 8px;
+
+  background-color: ${({ theme }) => theme.colors.grayScaleWhite};
+`;
+
+const CloseIcon = styled(CloseIc)`
+  position: absolute;
+  top: 1rem;
+  right: 1.1rem;
+`;
+
+const BtnModalTitle = styled.h2`
+  ${({ theme }) => theme.fonts.Head2_SB_18};
+  color: ${({ theme }) => theme.colors.grayScaleBG};
+`;
+
+const BtnModalBtn = styled.button`
+  width: 100%;
+  padding: 1.25rem;
+  border-radius: 8px;
+
+  ${({ theme }) => theme.fonts.Head2_SB_18};
+  background-color: ${({ theme }) => theme.colors.Blue};
+
+  color: ${({ theme }) => theme.colors.grayScaleWhite};
+
+  cursor: pointer;
+`;
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,5 +1,27 @@
+import { useState } from 'react';
+import { BtnCloseModal } from '../../components/commons/Modal';
+
 const HomePage = () => {
-  return <div>Home</div>;
+  const [showModal, setShowModal] = useState(false);
+
+  const onClickBtn = () => {
+    setShowModal(true);
+  };
+  const handleShowModal = (type: boolean) => {
+    setShowModal(type);
+  };
+
+  return (
+    <>
+      <button onClick={onClickBtn}>모달 여는 버튼</button>
+      <BtnCloseModal
+        showModal={showModal}
+        handleShowModal={handleShowModal}
+        title="약속 잡기 전 주의해주세요"
+        btnText="확인했어요"
+      />
+    </>
+  );
 };
 
 export default HomePage;
