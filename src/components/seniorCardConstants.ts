6a0920fc612b2ac8622f9faca6e4a773599a8f5a
yarimu <yhli0320@naver.com>
2024-07-08 17:50:31 +0900

    feat: 선배프로필 목데이터

---
+++ src/components/seniorCardConstants.ts
@@ -0,0 +1,28 @@
+export const SENIOR_LIST = {
+  seniorList: [
+    {
+      seniorId: 1,
+      nickname: '범이에요',
+      company: '비바리퍼블리카(토스)',
+      position: '백엔드 개발자',
+      field: '공학 계열',
+      level: 5,
+    },
+    {
+      seniorId: 2,
+      nickname: '김도현',
+      company: '선약주식회사',
+      position: '마케터',
+      field: '인문 계열',
+      level: 2,
+    },
+    {
+      seniorId: 3,
+      nickname: '숭퍼노바',
+      company: '선약콤퓨타',
+      position: '프론트엔드 개발자',
+      field: '예체능 계열',
+      level: 124,
+    },
+  ],
+};
