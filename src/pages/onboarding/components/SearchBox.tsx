8626dbaa3129c40a27da60ef9ece10aad27cbc24
lydiacho <sheecho0804@ewhain.net>
2024-07-08 19:24:46 +0900

    design: Input 퍼블리싱

--- src/pages/onboarding/components/SearchBox.tsx
+++ src/pages/onboarding/components/SearchBox.tsx
@@ -11,6 +11,18 @@ const SearchBox = () => {
 export default SearchBox;
 
 const Input = styled.input`
-  display: flex;
-  align-items: center;
+  width: 100%;
+  height: 5.1rem;
+  padding: 1rem 1.5rem;
+  ${({ theme }) => theme.fonts.Title2_M_16};
+  border: none;
+  border-radius: 8px;
+
+  background-color: ${({ theme }) => theme.colors.grayScaleLG1};
+
+  color: ${({ theme }) => theme.colors.grayScaleBG};
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.grayScaleMG2};
+  }
 `;
