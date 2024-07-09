fdfafdfc084c96cceb7ebec25caeb6747ba74f5e
se0jinYoon <tjwls1999@gmail.com>
2024-07-09 23:30:28 +0900

    chore: 예시 코드 삭제

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,20 +1,5 @@
-import { useState } from 'react';
-import { BtnCloseModal } from '../../components/commons/modal/BtnModal';
-
 const HomePage = () => {
-  const [showModal, setShowModal] = useState(false);
-
-  const onClickModal = () => {
-    setShowModal(false);
-  };
-  return (
-    <>
-      <button type="button" onClick={() => setShowModal(true)}>
-        열기
-      </button>
-      <BtnCloseModal title="테스트" showModal={showModal} handleShowModal={onClickModal} btnText="닫기" />
-    </>
-  );
+  return <div>home</div>;
 };
 
 export default HomePage;
