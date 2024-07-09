7ff0540259c236a1a01cde711b6a5ea5b33e1260
Jieun Lee <najee0610@naver.com>
2024-07-10 05:15:27 +0900

    feat: 고민 선택/작성 버튼 추가

--- src/pages/juniorPromise/JuniorPromisePage.tsx
+++ src/pages/juniorPromise/JuniorPromisePage.tsx
@@ -1,12 +1,27 @@
+import ToggleButton from '@components/commons/ToggleButton';
 import styled from '@emotion/styled';
+import { useState } from 'react';
+import SelectJuniorWorryButton from './components/SelectJuniorWorryButton';
+import SelectJuniorWorryTextBoxWrapper from './components/SelectJuniorWorryTextareaWrapper';
+import SelectJuniorWorryTitleWrapper from './components/SelectJuniorWorryTitleWrapper';
 import TimeSelectionButton from './components/TimeSelectionButton';
 import TimeSelectionTitleWrapper from './components/TimeSelectionTitleWrapper';
 
 const JuniorPromisePage = () => {
+  const [activeButton, setActiveButton] = useState<'left' | 'right'>('left');
+
+  // onToggle 함수 정의
+  const handleToggle = (button: 'left' | 'right') => {
+    setActiveButton(button);
+  };
+
   return (
     <TimeSelectionContainer>
       <TimeSelectionTitleWrapper />
       <TimeSelectionButton />
+      <SelectJuniorWorryTitleWrapper />
+      <ToggleButton left="선택할게요" right="작성할게요" activeButton={activeButton} onToggle={handleToggle} />
+      {activeButton === 'left' ? <SelectJuniorWorryButton /> : <SelectJuniorWorryTextBoxWrapper />}
     </TimeSelectionContainer>
   );
 };
