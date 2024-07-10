bb60938be3ee5d009b37370ae584db881a147739
lydiacho <sheecho0804@ewhain.net>
2024-07-10 22:28:33 +0900

    fix: Caption 위치 이동

--- src/pages/onboarding/components/seniorOnboarding/Step졸업인증.tsx
+++ src/pages/onboarding/components/seniorOnboarding/Step졸업인증.tsx
@@ -6,8 +6,8 @@ const Step졸업인증 = () => {
       <TextBox label="졸업증명서">
         <InputBox label="졸업증명서" type="file" placeholder="파일 첨부하기">
           <InnerButton text="첨부파일" />
-          <Caption>JPEG, JPG, PNG, PDF 형식만 첨부 가능해요 (최대 50BM)</Caption>
         </InputBox>
+        <Caption>JPEG, JPG, PNG, PDF 형식만 첨부 가능해요 (최대 50BM)</Caption>
       </TextBox>
     </>
   );
