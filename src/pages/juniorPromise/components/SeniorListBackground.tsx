6e34abb3db9b1cd0c91510661f2d0226fc6849d7
yarimu <yhli0320@naver.com>
2024-07-11 04:33:05 +0900

    feat: 필터버튼 컴포넌트 분리, fragment 삭제

--- src/pages/juniorPromise/components/SeniorListBackground.tsx
+++ src/pages/juniorPromise/components/SeniorListBackground.tsx
@@ -1,31 +1,20 @@
 import styled from '@emotion/styled';
-import { ArrowDownIc, ResetIc, Line292Ic } from '../../../assets/svgs/index';
-
+import { FilterButton } from './FilterButton';
+import { ResetIc, Line292Ic } from '../../../assets/svgs/index';
 export const SeniorListBackground = () => {
   return (
-    <>
-      <ListBackground>
-        <SeniorSearchWrapper>
-          <SearchTitle>선배를 찾아볼까요?</SearchTitle>
-          <BtnLayout>
-            <FilterBtnContainer>
-              <FilterBtn type="button">
-                계열
-                <ArrowDownIc />
-              </FilterBtn>
-              <FilterBtn type="button">
-                직무
-                <ArrowDownIc />
-              </FilterBtn>
-            </FilterBtnContainer>
-            <LineWrapper>
-              <Line292Ic />
-            </LineWrapper>
-            <ResetIc />
-          </BtnLayout>
-        </SeniorSearchWrapper>
-      </ListBackground>
-    </>
+    <ListBackground>
+      <SeniorSearchWrapper>
+        <SearchTitle>선배를 찾아볼까요?</SearchTitle>
+        <BtnLayout>
+          <FilterButton />
+          <LineWrapper>
+            <Line292Ic />
+          </LineWrapper>
+          <ResetIc />
+        </BtnLayout>
+      </SeniorSearchWrapper>
+    </ListBackground>
   );
 };
 
@@ -58,23 +47,6 @@ const BtnLayout = styled.div`
   align-items: center;
 
   padding: 0.7rem 2rem;
-`;
-const FilterBtnContainer = styled.div`
-  display: flex;
-  gap: 0.8rem;
-`;
-const FilterBtn = styled.button`
-  display: flex;
-  gap: 1rem;
-  justify-content: center;
-  align-items: center;
-
-  width: 6.3rem;
-  padding: 0.8rem 1rem;
-  border-radius: 6px;
-
-  ${({ theme }) => theme.fonts.Caption2_SB_12};
-  background: ${({ theme }) => theme.colors.grayScaleLG2};
 `;
 
 const LineWrapper = styled.div`
