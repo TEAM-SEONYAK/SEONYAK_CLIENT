9633d476b43de42804a2fd970ab2dfec792739f4
yarimu <yhli0320@naver.com>
2024-07-11 02:01:53 +0900

    feat: 바텀시트 외부 클릭시 나가기 구현, 애니메이션 로직 변경

--- src/pages/juniorPromise/components/BottomSheetBg.tsx
+++ src/pages/juniorPromise/components/BottomSheetBg.tsx
@@ -1,7 +1,5 @@
 import { ReloadIc } from '@assets/svgs';
-import { keyframes } from '@emotion/react';
 import styled from '@emotion/styled';
-
 interface BottomSheetPropType {
   isSheetOpen: boolean;
   handleSheetOpen: () => void;
@@ -10,10 +8,9 @@ interface BottomSheetPropType {
 
 export const BottomSheetBg = ({ isSheetOpen, handleSheetOpen, handleSheetClose }: BottomSheetPropType) => {
   return (
-    <TestBg>
-      <TestBtn $isSheetOpen={isSheetOpen} onClick={handleSheetOpen}>
-        바텀시트 올리는 버튼
-      </TestBtn>
+    <>
+      <TestBtn onClick={handleSheetOpen}>바텀시트 올리는 버튼</TestBtn>
+      <TestBg $isSheetOpen={isSheetOpen} onClick={handleSheetClose} />
       <BottomSheetWrapper $isSheetOpen={isSheetOpen}>
         <TitleLayout>
           <Line />
@@ -29,49 +26,24 @@ export const BottomSheetBg = ({ isSheetOpen, handleSheetOpen, handleSheetClose }
           </ExitBottomSheet>
         </ButtonLayout>
       </BottomSheetWrapper>
-    </TestBg>
+    </>
   );
 };
 
-const TestBg = styled.div`
-  position: relative;
+const TestBg = styled.div<{ $isSheetOpen: boolean }>`
+  display: ${({ $isSheetOpen }) => ($isSheetOpen ? 'flex' : 'none')};
+  position: fixed;
+  top: 0;
   z-index: 2;
 
   width: 100%;
   height: 100dvh;
 
-  background: ${({ theme }) => theme.colors.Blue};
+  background: ${({ theme }) => theme.colors.transparentBlack_65};
 `;
 
-const TestBtn = styled.button<{ $isSheetOpen: boolean }>`
+const TestBtn = styled.button`
   display: flex;
-  display: ${({ $isSheetOpen }) => ($isSheetOpen ? 'none' : 'flex')};
-`;
-
-// 바텀시트 올라오는 애니메이션
-const SheetOpenAnimation = keyframes`
-  from {
-    transform: translateY(100%);
-    opacity: 0;
-  }
-
-  to {
-    transform: translateY(0);
-    opacity: 1;
-  }
-`;
-
-// 바텀시트 내려가는 애니메이션
-const SheetCloseAnimation = keyframes`
-  from {
-    transform: translateY(0);
-    opacity: 1;
-  }
-
-  to {
-    transform: translateY(100%);
-    opacity: 0;
-  }
 `;
 
 const BottomSheetWrapper = styled.form<{ $isSheetOpen: boolean }>`
@@ -79,7 +51,7 @@ const BottomSheetWrapper = styled.form<{ $isSheetOpen: boolean }>`
   flex-direction: column;
   position: fixed;
   bottom: 0;
-  z-index: 1;
+  z-index: 4;
 
   width: 100%;
   height: auto;
@@ -88,15 +60,12 @@ const BottomSheetWrapper = styled.form<{ $isSheetOpen: boolean }>`
   background: ${({ theme }) => theme.colors.grayScaleWhite};
 
   opacity: ${({ $isSheetOpen }) => ($isSheetOpen ? 1 : 0)};
-  animation: ${({ $isSheetOpen }) =>
-    $isSheetOpen ? `${SheetOpenAnimation} 250ms ease-out` : `${SheetCloseAnimation} 250ms ease-out`};
+  transform: translateY(${({ $isSheetOpen }) => ($isSheetOpen ? '0' : '100%')});
+
   transition:
     transform 250ms ease-out,
     opacity 250ms ease-out;
-
-  transform: ${({ $isSheetOpen }) => ($isSheetOpen ? 'translateY(0)' : 'translateY(100%)')};
 `;
-
 const TitleLayout = styled.header`
   padding: 1.4rem 16.1rem 1.6rem 2rem;
 `;
@@ -125,6 +94,7 @@ const ButtonLayout = styled.footer`
   justify-content: center;
   align-items: center;
 
+  margin-bottom: 2.6rem;
   padding: 0.3rem 2rem;
 `;
 const ReloadIcon = styled.button`
@@ -147,155 +117,3 @@ const ExitBottomSheet = styled.button`
 
   ${({ theme }) => theme.fonts.Head2_SB_18};
 `;
-
-// import { ReloadIc } from '@assets/svgs';
-// import { keyframes } from '@emotion/react';
-// import styled from '@emotion/styled';
-
-// interface BottomSheetPropType {
-//   isSheetOpen: boolean;
-//   handleSheetOpen: () => void;
-//   handleSheetClose: () => void;
-// }
-
-// export const BottomSheetBg = ({ isSheetOpen, handleSheetOpen, handleSheetClose }: BottomSheetPropType) => {
-//   return (
-//     <TestBg>
-//       <TestBtn $isSheetOpen={isSheetOpen} onClick={handleSheetOpen}>
-//         바텀시트 올리는 버튼
-//       </TestBtn>
-//       <BottomSheetWrapper $isSheetOpen={isSheetOpen}>
-//         <TitleLayout>
-//           <Line />
-//           <Title>원하는 선배를 찾아볼까요?</Title>
-//           <Desc>계열, 직무로 원하는 선배를 찾을 수 있어요.</Desc>
-//         </TitleLayout>
-//         <ButtonLayout>
-//           <ReloadIcon type="reset">
-//             <ReloadIc />
-//           </ReloadIcon>
-//           <ExitBottomSheet type="button" onClick={handleSheetClose}>
-//             적용할래요
-//           </ExitBottomSheet>
-//         </ButtonLayout>
-//       </BottomSheetWrapper>
-//     </TestBg>
-//   );
-// };
-
-// const TestBg = styled.div`
-//   position: relative;
-//   z-index: 2;
-
-//   width: 100%;
-//   height: 100dvh;
-
-//   background: ${({ theme }) => theme.colors.Blue};
-// `;
-
-// const TestBtn = styled.button<{ $isSheetOpen: boolean }>`
-//   display: flex;
-//   display: ${({ $isSheetOpen }) => ($isSheetOpen ? 'none' : 'flex')};
-// `;
-
-// // 바텀시트 올라오는 애니메이션
-// const SheetOpenAnimation = keyframes`
-//   from {
-//     transform: translateY(100%);
-//     opacity: 0;
-//   }
-
-//   to {
-//     transform: translateY(0);
-//     opacity: 1;
-//   }
-// `;
-
-// // 바텀시트 내려가는 애니메이션
-// const SheetCloseAnimation = keyframes`
-//   from {
-//     transform: translateY(0);
-//     opacity: 1;
-//   }
-
-//   to {
-//     transform: translateY(100%);
-//     opacity: 0;
-//   }
-// `;
-
-// const BottomSheetWrapper = styled.form<{ $isSheetOpen: boolean }>`
-//   display: flex;
-//   flex-direction: column;
-
-//   /* gap: 43.5rem; */
-//   position: bottom;
-//   top: 0;
-//   z-index: 1;
-
-//   width: 100%;
-//   height: auto;
-//   border-radius: 16px 16px 0 0;
-
-//   background: ${({ theme }) => theme.colors.grayScaleWhite};
-
-//   opacity: ${({ $isSheetOpen }) => ($isSheetOpen ? 1 : 0)};
-//   animation: ${({ $isSheetOpen }) =>
-//     $isSheetOpen ? `${SheetOpenAnimation} 250ms ease-out` : `${SheetCloseAnimation} 250ms ease-out`};
-//   transition:
-//     transform 250ms ease-out,
-//     opacity 250ms ease-out;
-
-//   transform: ${({ $isSheetOpen }) => ($isSheetOpen ? 'translateY(0)' : 'translateY(100%)')};
-// `;
-
-// const TitleLayout = styled.header`
-//   padding: 1.4rem 16.1rem 1.6rem 2rem;
-// `;
-
-// const Line = styled.div`
-//   width: 4.7rem;
-//   height: 0.3rem;
-//   margin: 0 14.4rem 3.3rem;
-//   border-radius: 5px;
-
-//   background: ${({ theme }) => theme.colors.grayScaleLG2};
-// `;
-// const Title = styled.h1`
-//   ${({ theme }) => theme.fonts.Head2_SB_18};
-//   color: ${({ theme }) => theme.colors.grayScaleBG};
-// `;
-
-// const Desc = styled.p`
-//   ${({ theme }) => theme.fonts.Caption1_R_12};
-//   color: ${({ theme }) => theme.colors.grayScaleMG1};
-// `;
-
-// const ButtonLayout = styled.footer`
-//   display: flex;
-//   gap: 1.1rem;
-//   justify-content: center;
-//   align-items: center;
-
-//   padding: 0.3rem 2rem;
-// `;
-// const ReloadIcon = styled.button`
-//   width: 5rem;
-//   height: 5rem;
-//   padding: 1.8rem 1.8rem 1.898rem 1.9rem;
-//   border-radius: 8px;
-
-//   background: ${({ theme }) => theme.colors.grayScaleLG2};
-// `;
-
-// const ExitBottomSheet = styled.button`
-//   width: 27.4rem;
-//   height: 5rem;
-//   border-radius: 8px;
-
-//   background: ${({ theme }) => theme.colors.grayScaleMG1};
-
-//   color: ${({ theme }) => theme.colors.grayScaleWhite};
-
-//   ${({ theme }) => theme.fonts.Head2_SB_18};
-// `;
