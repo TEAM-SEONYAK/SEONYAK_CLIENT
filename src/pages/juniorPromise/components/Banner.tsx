338c9195a4dfc193fbfe7696f69ec93e0b4868c5
Jieun Lee <najee0610@naver.com>
2024-07-10 18:57:21 +0900

    design: 후배 약속신청 뷰 배너 퍼블리싱

---
+++ src/pages/juniorPromise/components/Banner.tsx
@@ -0,0 +1,48 @@
+import { TechCharacter } from '@assets/svgs';
+import styled from '@emotion/styled';
+import React from 'react';
+
+interface SeniorProps {
+  senior: string;
+}
+
+// 로그인 기능 붙여지면 선배 이름 가져오기 수정
+const Banner: React.FC<SeniorProps> = ({ senior = '도리 선배' }) => {
+  return (
+    <BannerWrapper>
+      <Text>
+        <SeniorText>{senior}</SeniorText> 님에게
+        <br />
+        약속 신청을 시작할게요
+      </Text>
+      <CustomTechCharacter />
+    </BannerWrapper>
+  );
+};
+
+export default Banner;
+
+const BannerWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 100%;
+  padding: 1rem;
+`;
+
+const Text = styled.div`
+  ${({ theme }) => theme.fonts.Head1_SB_20};
+  padding-left: 2rem;
+
+  color: ${({ theme }) => theme.colors.grayScaleBG};
+`;
+
+const SeniorText = styled.span`
+  color: ${({ theme }) => theme.colors.Blue};
+`;
+
+const CustomTechCharacter = styled(TechCharacter)`
+  width: 13.9rem;
+  height: 11.8rem;
+`;
