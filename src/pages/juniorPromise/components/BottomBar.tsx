d02e60fcefff80c9377588e0e6f5a0886da43d2c
Jieun Lee <najee0610@naver.com>
2024-07-11 23:57:29 +0900

    design: 바텀바 퍼블리싱

---
+++ src/pages/juniorPromise/components/BottomBar.tsx
@@ -0,0 +1,63 @@
+import { ReloadIc } from '@assets/svgs';
+import styled from '@emotion/styled';
+
+interface BottomSheetPropType {
+  handleSheetOpen: () => void;
+  handleSheetClose: () => void;
+}
+
+const BottomBar = ({ handleSheetClose }: BottomSheetPropType) => {
+  return (
+    <ButtonLayout>
+      <ReloadBtn type="reset">
+        <ReloadIcon />
+      </ReloadBtn>
+      <ExitBottomSheet type="button" onClick={handleSheetClose}>
+        적용하기
+      </ExitBottomSheet>
+    </ButtonLayout>
+  );
+};
+
+export default BottomBar;
+
+const ReloadIcon = styled(ReloadIc)`
+  width: 2rem;
+  height: 2rem;
+`;
+
+const ButtonLayout = styled.footer`
+  display: flex;
+  gap: 1.1rem;
+  justify-content: center;
+
+  height: 9.4rem;
+  margin-bottom: 2.6rem;
+  padding: 1.5rem 0 3rem;
+
+  box-shadow: 0 -4px 8px rgb(0 0 0 / 10%);
+`;
+
+const ReloadBtn = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 5rem;
+  height: 5rem;
+  border-radius: 8px;
+
+  background: ${({ theme }) => theme.colors.grayScaleLG2};
+`;
+
+const ExitBottomSheet = styled.button`
+  width: 27.4rem;
+  height: 5rem;
+  border-radius: 8px;
+
+  background: ${({ theme }) => theme.colors.grayScaleMG1};
+
+  color: ${({ theme }) => theme.colors.grayScaleWhite};
+
+  ${({ theme }) => theme.fonts.Head2_SB_18};
+`;
