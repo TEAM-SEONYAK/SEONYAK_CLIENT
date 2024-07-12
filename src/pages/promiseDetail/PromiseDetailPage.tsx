e585ec48c58d59efd22cf94ee33421309c621e6f
se0jinYoon <tjwls1999@gmail.com>
2024-07-13 07:13:25 +0900

    feat: api 연결 변수 지정

--- src/pages/promiseDetail/PromiseDetailPage.tsx
+++ src/pages/promiseDetail/PromiseDetailPage.tsx
@@ -1,30 +1,39 @@
-import { ArrowLeftIc } from '@assets/svgs';
+import { ArrowLeftIc, ButtonCheckIc } from '@assets/svgs';
 import { Header } from '@components/commons/Header';
 import styled from '@emotion/styled';
+import { useState } from 'react';
+import { SENIOR_RESPONSE, JUNIOR_RESPONSE } from './constants/constant';
+import { formatDate } from './utils/formatDate';
 
 const PromiseDetail = () => {
+  // location으로 닉네임 잡아오기
+  const myNickname = '아가라고요';
+  const userRole = 'SENIOR';
+  const [selectTime, setSelectTime] = useState('');
+
   return (
     <>
       <Header LeftSvg={ArrowLeftIc} title="자세히 보기" />
       <Wrapper>
         <Layout>
           <TitleContainer>
-            <Title>아가라고요 후배님의 정보</Title>
-            <Content>홍익대학교 조형대학 디자인컨버전스학부</Content>
+            <Title>
+              {SENIOR_RESPONSE.juniorInfo.nickname} {userRole === 'SENIOR' ? '후배' : '선배'}님의 정보
+            </Title>
+            <Content>
+              {SENIOR_RESPONSE.juniorInfo.univName} {SENIOR_RESPONSE.juniorInfo.field}{' '}
+              {SENIOR_RESPONSE.juniorInfo.department}
+            </Content>
           </TitleContainer>
 
           <TitleContainer>
-            <Title>예솔 선배님과 상담하고 싶은 내용</Title>
+            <Title>{myNickname} 선배님과 상담하고 싶은 내용</Title>
             <ContentContainer>
-              <Content>면접에 관해서 물어보고 싶어요</Content>
-              <Content>면접에 관해서 물어보고 싶어요</Content>
-              <Content>면접에 관해서 물어보고 싶어요</Content>
-              {/* <WrittenContent>
-                저는 기술적 전문성과 혁신적인 아이디어로 고객의 니즈를 해결하는 개발자입니다. 오랜 기간 쌓아온 경험과
-                노하우를 바탕으로 고객님의 요구사항을 신속하고 정확하게 파악하여 최적의 솔루션을 제공해 드리겠습니다.
-                마치 제가 직접 운영하는 가게처럼 열정을 다해 고객 맞춤형 서비스를 설계하겠습니다.는 가게처럼 열정을 다해
-                고객 맞춤형 서비스를 설계하겠습니다.습니다.다
-              </WrittenContent> */}
+              {SENIOR_RESPONSE.topic.length ? (
+                SENIOR_RESPONSE.topic.map((el, idx) => <Content key={idx + el}>{el}</Content>)
+              ) : (
+                <WrittenContent>{SENIOR_RESPONSE.personalTopic}</WrittenContent>
+              )}
             </ContentContainer>
           </TitleContainer>
 
@@ -32,9 +41,11 @@ const PromiseDetail = () => {
             <Title>희망하는 약속 시간</Title>
             <Description>세 가지 시간 중 하나를 필수로 선택해주세요</Description>
             <ContentContainer>
-              <Content>2024년 7월 7일 20:30 - 21:00</Content>
-              <Content>2024년 7월 7일 20:30 - 21:00</Content>
-              <Content>2024년 7월 7일 20:30 - 21:00</Content>
+              {SENIOR_RESPONSE.timeList.map((el, idx) => (
+                <Time key={el.date + idx + el.startTime}>
+                  {formatDate(el.date)} {el.startTime} - {el.endTime}
+                </Time>
+              ))}
             </ContentContainer>
           </TimeContainer>
         </Layout>
@@ -92,6 +103,15 @@ const ContentContainer = styled.div`
 `;
 
 const Content = styled.div`
+  width: 100%;
+  padding: 1.1rem 0 1.1rem 1.5rem;
+  border-radius: 8px;
+  background-color: ${({ theme }) => theme.colors.grayScaleLG1};
+  color: ${({ theme }) => theme.colors.grayScaleBG};
+  ${({ theme }) => theme.fonts.Body1_M_14}
+`;
+
+const Time = styled.div`
   width: 100%;
   padding: 1.1rem 0 1.1rem 1.5rem;
   border-radius: 8px;
---
+++ src/pages/promiseDetail/constants/constant.ts
@@ -0,0 +1,65 @@
+export const SENIOR_RESPONSE = {
+  juniorInfo: {
+    nickname: '김도현',
+    univName: '선약대학교',
+    field: '선약계열',
+    department: '선약학과',
+  },
+  topic: [],
+  personalTopic:
+    '저는 기술적 전문성과 혁신적인 아이디어로 고객의 니즈를 해결하는 개발자입니다. 오랜 기간 쌓아온 경험과 노하우를 바탕으로 고객님의 요구사항을 신속하고 정확하게 파악하여 최적의 솔루션을 제공해 드리겠습니다. 마치 제가 직접 운영하는 가게처럼 열정을 다해 고객 맞춤형 서비스를 설계하겠습니다.는 가게처럼 열정을 다해 고객 맞춤형 서비스를 설계하겠습니다.습니다.다',
+  timeList: [
+    {
+      date: '2024.07.03',
+      startTime: '12:00',
+      endTime: '12:30',
+    },
+    {
+      date: '2024.07.04',
+      startTime: '13:00',
+      endTime: '13:30',
+    },
+    {
+      date: '2024.07.05',
+      startTime: '18:00',
+      endTime: '18:30',
+    },
+  ],
+};
+
+export const JUNIOR_RESPONSE = {
+  seniorInfo: {
+    nickname: '조승희',
+    image: 'https://example.com/senior1.jpg',
+    company: 'SOPT Makers',
+    field: '공학계열',
+    position: '개발',
+    detailPosition: 'FE챕터장',
+    level: '3년차',
+  },
+  juniorInfo: {
+    nickname: '김도현',
+    univName: '선약대학교',
+    field: '선약계열',
+    department: '선약학과',
+  },
+  topic: ['포트폴리오', '면접 준비', '커리어 로드맵', '자소서 컨설팅'],
+  personalTopic: '',
+  timeList: [
+    {
+      date: '2024.07.03',
+      startTime: '12:00',
+      endTime: '12:30',
+    },
+    {
+      date: '2024.07.04',
+      startTime: '13:00',
+      endTime: '13:30',
+    },
+    {
+      date: '2024.07.05',
+      startTime: '18:00',
+      endTime: '18:30',
+    },
+  ],
+};
