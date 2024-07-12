1898180c1b1ca343b241a201c4d1ee70c26a353e
Jieun Lee <najee0610@naver.com>
2024-07-12 22:25:54 +0900

    feat: 모달 오픈 추가

--- src/pages/juniorPromise/components/CheckModal.tsx
+++ src/pages/juniorPromise/components/CheckModal.tsx
@@ -1,18 +1,28 @@
 import { BtnCloseModal } from '@components/commons/modal/BtnModal';
 import styled from '@emotion/styled';
+import { useState } from 'react';
 import CheckModalContent from './CheckModalContent';
 
 const CheckModal = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleModalOpen = (isOpen: boolean) => {
+    setIsModalOpen(isOpen);
+  };
+
   return (
-    <Wrapper>
-      <BtnCloseModal
-        title={'약속 잡기 전 주의해주세요'}
-        isModalOpen={true}
-        handleModalOpen={() => {}}
-        btnText={'적용할래요'}>
-        <CheckModalContent />
-      </BtnCloseModal>
-    </Wrapper>
+    <>
+      <button onClick={() => handleModalOpen(true)}>모달 열기</button>
+      <Wrapper>
+        <BtnCloseModal
+          title={'약속 잡기 전 주의해주세요'}
+          isModalOpen={isModalOpen}
+          handleModalOpen={handleModalOpen}
+          btnText={'적용할래요'}>
+          <CheckModalContent />
+        </BtnCloseModal>
+      </Wrapper>
+    </>
   );
 };
 
@@ -22,9 +32,8 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  position: relative;
-  bottom: 19.4rem;
+  position: absolute;
+  top: 19.4rem;
 
-  width: 100%;
-  height: 100%;
+  border-radius: 8px;
 `;
