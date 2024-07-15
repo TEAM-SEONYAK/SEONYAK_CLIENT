d0b0af9458b63e0973f4148913cb1aecf54f7524
yarimu <yhli0320@naver.com>
2024-07-16 03:40:02 +0900

    fix: 직무 계열리스트 구조 변경

--- src/pages/juniorPromise/constants/positionList.ts
+++ src/pages/juniorPromise/constants/positionList.ts
@@ -1,88 +1,86 @@
-export const POSITION_LIST = {
-  positionList: [
-    {
-      id: 1,
-      position: '경영 ・비지니스',
-    },
-    {
-      id: 2,
-      position: '서비스 기획 ・ 운영',
-    },
-    {
-      id: 3,
-      position: '개발',
-    },
-    {
-      id: 4,
-      position: '데이터 ・ AI ・ ML',
-    },
-    {
-      id: 5,
-      position: '마케팅 ・ 광고 ・ 홍보',
-    },
-    {
-      id: 6,
-      position: '디자인',
-    },
-    {
-      id: 7,
-      position: '미디어 ・ 커뮤니케이션',
-    },
-    {
-      id: 8,
-      position: '이커머스 ・ 리테일',
-    },
-    {
-      id: 9,
-      position: '금융 ・ 컨설팅 ・ VC',
-    },
-    {
-      id: 10,
-      position: '회계 ・ 재무',
-    },
-    {
-      id: 11,
-      position: '인사 ・ 채용 ・ 노무',
-    },
-    {
-      id: 12,
-      position: '고객 ・ 영업',
-    },
-    {
-      id: 13,
-      position: '게임 기획 ・ 개발',
-    },
-    {
-      id: 14,
-      position: '물류 ・ 구매',
-    },
-    {
-      id: 15,
-      position: '의료 ・ 제약 ・ 바이오',
-    },
-    {
-      id: 16,
-      position: '연구 ・ R&B',
-    },
-    {
-      id: 17,
-      position: '엔지니어링 ・ 설계',
-    },
-    {
-      id: 18,
-      position: '생산 ・ 품질',
-    },
-    {
-      id: 19,
-      position: '교육B',
-    },
-    {
-      id: 20,
-      position: '법률 ・ 특허',
-    },
-    {
-      id: 21,
-      position: '공공 ・ 복지 ・ 환경',
-    },
-  ],
-};
+export const POSITION_LIST = [
+  {
+    id: 1,
+    position: '경영 ・비지니스',
+  },
+  {
+    id: 2,
+    position: '서비스 기획 ・ 운영',
+  },
+  {
+    id: 3,
+    position: '개발',
+  },
+  {
+    id: 4,
+    position: '데이터 ・ AI ・ ML',
+  },
+  {
+    id: 5,
+    position: '마케팅 ・ 광고 ・ 홍보',
+  },
+  {
+    id: 6,
+    position: '디자인',
+  },
+  {
+    id: 7,
+    position: '미디어 ・ 커뮤니케이션',
+  },
+  {
+    id: 8,
+    position: '이커머스 ・ 리테일',
+  },
+  {
+    id: 9,
+    position: '금융 ・ 컨설팅 ・ VC',
+  },
+  {
+    id: 10,
+    position: '회계 ・ 재무',
+  },
+  {
+    id: 11,
+    position: '인사 ・ 채용 ・ 노무',
+  },
+  {
+    id: 12,
+    position: '고객 ・ 영업',
+  },
+  {
+    id: 13,
+    position: '게임 기획 ・ 개발',
+  },
+  {
+    id: 14,
+    position: '물류 ・ 구매',
+  },
+  {
+    id: 15,
+    position: '의료 ・ 제약 ・ 바이오',
+  },
+  {
+    id: 16,
+    position: '연구 ・ R&B',
+  },
+  {
+    id: 17,
+    position: '엔지니어링 ・ 설계',
+  },
+  {
+    id: 18,
+    position: '생산 ・ 품질',
+  },
+  {
+    id: 19,
+    position: '교육B',
+  },
+  {
+    id: 20,
+    position: '법률 ・ 특허',
+  },
+  {
+    id: 21,
+    position: '공공 ・ 복지 ・ 환경',
+  },
+];
