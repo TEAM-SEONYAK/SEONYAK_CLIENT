ce69210080d8fda193250a60797bdeee1766c3ea
lydiacho <sheecho0804@ewhain.net>
2024-07-16 05:20:20 +0900

    feat: 인증모달 구현

--- src/pages/onboarding/components/seniorOnboarding/Step졸업인증.tsx
+++ src/pages/onboarding/components/seniorOnboarding/Step졸업인증.tsx
@@ -17,27 +17,32 @@ const Step졸업인증 = () => {
     setSuccess(type);
   };
   return (
-    <Wrapper>
-      <TextBox label="졸업증명서">
-        <InputBox
-          label="졸업증명서"
-          type="file"
-          onChange={handleChangeFile}
-          text={fileName}
-          placeholder={DEFAULT_TEXT}
-          isError={isError}>
-          <InnerButton text="첨부파일" />
-        </InputBox>
-        {isError ? (
-          <WarnDescription isShown={isError} warnText="인증에 실패했어요. 학교명이 잘 보이는 지 확인해 주세요." />
-        ) : (
-          <Caption>JPEG, JPG, PNG, PDF 형식만 첨부 가능해요 (최대 50MB)</Caption>
-        )}
-      </TextBox>
-      <AutoCloseModal text="인증에 성공했어요" showModal={isSuccess} handleShowModal={handleSetSuccess}>
-        <Dummy />
-      </AutoCloseModal>
-    </Wrapper>
+    <>
+      <Wrapper>
+        <TextBox label="졸업증명서">
+          <InputBox
+            label="졸업증명서"
+            type="file"
+            onChange={handleChangeFile}
+            text={fileName}
+            placeholder={DEFAULT_TEXT}
+            isError={isError}>
+            <InnerButton text="첨부파일" />
+          </InputBox>
+          {isError ? (
+            <WarnDescription isShown={isError} warnText="인증에 실패했어요. 학교명이 잘 보이는 지 확인해 주세요." />
+          ) : (
+            <Caption>JPEG, JPG, PNG, PDF 형식만 첨부 가능해요 (최대 50MB)</Caption>
+          )}
+        </TextBox>
+      </Wrapper>
+
+      <ModalWrapper>
+        <AutoCloseModal text="인증에 성공했어요" showModal={isSuccess} handleShowModal={handleSetSuccess}>
+          <Dummy />
+        </AutoCloseModal>
+      </ModalWrapper>
+    </>
   );
 };
 
@@ -52,4 +57,10 @@ const Dummy = styled.div`
   height: 17rem;
 
   background-color: aliceblue;
+`;
+
+const ModalWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  margin-left: -2rem;
 `;
