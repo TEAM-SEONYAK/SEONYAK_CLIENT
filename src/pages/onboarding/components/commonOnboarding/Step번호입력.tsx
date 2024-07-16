242870f7969c0a67b20e414bd3006b9ddccc3ead
lydiacho <sheecho0804@ewhain.net>
2024-07-17 01:50:06 +0900

    feat: 에러케이스 구현

--- src/pages/onboarding/components/commonOnboarding/Step번호입력.tsx
+++ src/pages/onboarding/components/commonOnboarding/Step번호입력.tsx
@@ -6,8 +6,12 @@ import { useContext } from 'react';
 import { StepContext } from '@pages/onboarding/OnboardingPage';
 import styled from '@emotion/styled';
 import { formatPhone } from '@pages/onboarding/utils/formatPhone';
+import WarnDescription from '@components/commons/WarnDescription';
 
 const Step번호입력 = () => {
+  const [isNumError, setIsNumError] = useState(false);
+  const [isValidCodeError, setIsValidCodeError] = useState(false);
+
   const [phoneNumber, setPhoneNumber] = useState('');
   const { onNext } = useContext(StepContext);
   // 임시 변수
@@ -47,19 +51,30 @@ const Step번호입력 = () => {
   return (
     <Wrapper>
       <TextBox label="">
-        <InputBox
-          label="폰번호"
-          placeholder="전화번호를 입력해 주세요"
-          value={phoneNumber}
-          onChange={handleChangePhone}>
-          <InnerButton onClick={handleClickTimer} text={isActive ? '재전송' : '인증번호 전송'} />
-        </InputBox>
-        {isActive && (
-          <InputBox label="인증번호" placeholder="전송된 4자리 코드를 입력해 주세요" maxLength={4}>
-            <Timer>
-              {minutes} : {seconds}
-            </Timer>
+        <>
+          <InputBox
+            label="폰번호"
+            placeholder="전화번호를 입력해 주세요"
+            value={phoneNumber}
+            onChange={handleChangePhone}
+            isError={isNumError}>
+            <InnerButton onClick={handleClickTimer} text={isActive ? '재전송' : '인증번호 전송'} />
           </InputBox>
+          <WarnDescription isShown={isNumError} warnText="이미 사용 중인 번호예요." />
+        </>
+        {isActive && (
+          <>
+            <InputBox
+              label="인증번호"
+              placeholder="전송된 4자리 코드를 입력해 주세요"
+              maxLength={4}
+              isError={isValidCodeError}>
+              <Timer>
+                {minutes} : {seconds}
+              </Timer>
+            </InputBox>
+            <WarnDescription isShown={isValidCodeError} warnText="코드가 틀렸어요. 다시 한번 확인해 주세요." />
+          </>
         )}
       </TextBox>
       <FullBtn text="인증 확인" isActive={timeLeft > 0 && USER_INPUT === VERIFICATION_CODE} onClick={onNext} />
--- src/pages/onboarding/components/juniorOnboarding/Step이메일입력.tsx
+++ src/pages/onboarding/components/juniorOnboarding/Step이메일입력.tsx
@@ -5,8 +5,12 @@ import { InnerButton, InputBox, TextBox } from '../TextBox';
 import { useContext } from 'react';
 import { StepContext } from '@pages/onboarding/OnboardingPage';
 import styled from '@emotion/styled';
+import WarnDescription from '@components/commons/WarnDescription';
 
 const Step이메일입력 = () => {
+  const [isEmailError, setIsEmailError] = useState(false);
+  const [isValidCodeError, setIsValidCodeError] = useState(false);
+
   const [email, setEmail] = useState('');
   const { onNext } = useContext(StepContext);
   // 임시 변수
@@ -41,19 +45,30 @@ const Step이메일입력 = () => {
   return (
     <Wrapper>
       <TextBox label="">
-        <InputBox
-          label="학교메일"
-          placeholder="메일 주소를 입력해 주세요"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}>
-          <InnerButton onClick={handleClickTimer} text={isActive ? '재전송' : '인증번호 전송'} />
-        </InputBox>
-        {isActive && (
-          <InputBox label="인증번호" placeholder="전송된 4자리 코드를 입력해 주세요" maxLength={4}>
-            <Timer>
-              {minutes} : {seconds}
-            </Timer>
+        <>
+          <InputBox
+            label="학교메일"
+            placeholder="메일 주소를 입력해 주세요"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            isError={isEmailError}>
+            <InnerButton onClick={handleClickTimer} text={isActive ? '재전송' : '인증번호 전송'} />
           </InputBox>
+          <WarnDescription isShown={isEmailError} warnText="유효하지 않은 메일이에요." />
+        </>
+        {isActive && (
+          <>
+            <InputBox
+              label="인증번호"
+              placeholder="전송된 4자리 코드를 입력해 주세요"
+              maxLength={4}
+              isError={isValidCodeError}>
+              <Timer>
+                {minutes} : {seconds}
+              </Timer>
+            </InputBox>
+            <WarnDescription isShown={isValidCodeError} warnText="코드가 틀렸어요. 다시 한번 확인해 주세요." />
+          </>
         )}
       </TextBox>
       <FullBtn text="인증 확인" isActive={timeLeft > 0 && USER_INPUT === VERIFICATION_CODE} onClick={onNext} />
