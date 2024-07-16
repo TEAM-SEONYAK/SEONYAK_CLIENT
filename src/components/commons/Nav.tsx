e0c336476b907324318f83e64a22df07337b7813
Jieun Lee <najee0610@naver.com>
2024-07-17 03:57:00 +0900

    fix: nav 이동 버그 수정

--- src/components/commons/Nav.tsx
+++ src/components/commons/Nav.tsx
@@ -1,54 +1,54 @@
 import styled from '@emotion/styled';
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { NaviLookBlackIc, NaviPromiseBlackIc, NaviMyBlackIc } from '../../assets/svgs';
+
+type UserRole = 'JUNIOR' | 'SENIOR';
 
 const Nav = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
-  const userRole = 'JUNIOR';
-  const initialNav = userRole === 'JUNIOR' ? '둘러보기' : '나의 약속';
-  const [currNav, setCurrNav] = useState(initialNav);
+  const [userRole, setUserRole] = useState<UserRole>('JUNIOR');
+  const [currNav, setCurrNav] = useState(location.pathname);
+
+  useEffect(() => {
+    if (currNav !== location.pathname) {
+      navigate(currNav);
+    }
+  }, [currNav, navigate, location.pathname]);
 
   // 유저가 선배일 경우 접근 가능한 곳 : 나의 약속
   // 유저가 후배일 경우 접근 가능한 곳 : 둘러보기, 나의 약속
   // 접근 불가능한 곳은 클릭 안되고, 라우팅 처리 안됨
   const handleOnClickNav = (nav: string) => {
-    setCurrNav(nav);
-
-    switch (nav) {
-      case '둘러보기':
-        if (userRole === 'JUNIOR') {
-          navigate('/juniorPromise');
-        } else {
-          setCurrNav('나의 약속');
-          navigate('/');
-        }
-        break;
-      case '나의 약속':
-        setCurrNav('나의 약속');
-        navigate('/');
-        break;
-      case '마이페이지':
-        setCurrNav('마이페이지');
-        break;
-      default:
-        break;
+    if (userRole === 'JUNIOR') {
+      if (nav === '둘러보기') {
+        setCurrNav('/juniorPromise');
+      } else if (nav === '나의 약속') {
+        setCurrNav('/');
+      }
+    } else if (userRole === 'SENIOR') {
+      if (nav === '나의 약속') {
+        setCurrNav('/');
+      }
     }
   };
 
   return (
     <Wrapper>
-      <TapContainer onClick={() => handleOnClickNav('둘러보기')}>
-        <NaviLookBlackIcon isactive={(currNav === '둘러보기') + ''} />
-        <TapContent $isActive={currNav === '둘러보기'}>둘러보기</TapContent>
+      <TapContainer
+        onClick={() => userRole === 'JUNIOR' && handleOnClickNav('둘러보기')}
+        disabled={userRole === 'SENIOR'}>
+        <NaviLookBlackIcon isActive={currNav === '/juniorPromise'} />
+        <TapContent $isActive={currNav === '/juniorPromise'}>둘러보기</TapContent>
       </TapContainer>
       <TapContainer onClick={() => handleOnClickNav('나의 약속')}>
-        <NaviPromiseBlackIcon isactive={(currNav === '나의 약속') + ''} />
-        <TapContent $isActive={currNav === '나의 약속'}>나의 약속</TapContent>
+        <NaviPromiseBlackIcon isActive={currNav === '/'} />
+        <TapContent $isActive={currNav === '/'}>나의 약속</TapContent>
       </TapContainer>
-      <TapContainer onClick={() => handleOnClickNav('나의 약속')}>
-        <NaviMyBlackIcon isactive={(currNav === '마이페이지') + ''} />
+      <TapContainer onClick={() => userRole === 'JUNIOR' && handleOnClickNav('마이페이지')} disabled={true}>
+        <NaviMyBlackIcon isActive={currNav === '마이페이지'} />
         <TapContent $isActive={currNav === '마이페이지'}>마이페이지</TapContent>
       </TapContainer>
     </Wrapper>
@@ -70,14 +70,14 @@ const Wrapper = styled.nav`
   background-color: ${({ theme }) => theme.colors.grayScaleWhite};
 `;
 
-const TapContainer = styled.div`
+const TapContainer = styled.div<{ disabled?: boolean }>`
   display: flex;
   flex-direction: column;
   gap: 0.5rem;
   justify-content: center;
   align-items: center;
 
-  cursor: pointer;
+  cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
 `;
 
 const TapContent = styled.span<{ $isActive: boolean }>`
@@ -86,12 +86,14 @@ const TapContent = styled.span<{ $isActive: boolean }>`
   ${({ theme }) => theme.fonts.navigation};
 `;
 
-const NaviLookBlackIcon = styled(NaviLookBlackIc)<{ isactive: string }>`
-  stroke: ${({ isactive }) => (isactive === 'true' ? 'rgba(39, 39, 45, 1)' : 'rgba(197, 197, 197, 1)')};
+const NaviLookBlackIcon = styled(NaviLookBlackIc)<{ isActive: boolean }>`
+  stroke: ${({ isActive }) => (isActive ? 'rgba(39, 39, 45, 1)' : 'rgba(197, 197, 197, 1)')};
 `;
-const NaviPromiseBlackIcon = styled(NaviPromiseBlackIc)<{ isactive: string }>`
-  stroke: ${({ isactive }) => (isactive === 'true' ? 'rgba(39, 39, 45, 1)' : 'rgba(197, 197, 197, 1)')};
+
+const NaviPromiseBlackIcon = styled(NaviPromiseBlackIc)<{ isActive: boolean }>`
+  stroke: ${({ isActive }) => (isActive ? 'rgba(39, 39, 45, 1)' : 'rgba(197, 197, 197, 1)')};
 `;
-const NaviMyBlackIcon = styled(NaviMyBlackIc)<{ isactive: string }>`
-  stroke: ${({ isactive }) => (isactive === 'true' ? 'rgba(39, 39, 45, 1)' : 'rgba(197, 197, 197, 1)')};
+
+const NaviMyBlackIcon = styled(NaviMyBlackIc)<{ isActive: boolean }>`
+  stroke: ${({ isActive }) => (isActive ? 'rgba(39, 39, 45, 1)' : 'rgba(197, 197, 197, 1)')};
 `;
