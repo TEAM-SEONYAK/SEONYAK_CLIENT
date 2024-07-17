f3f9e7881e513118cf76fefcad586ee6579c6348
lydiacho <sheecho0804@ewhain.net>
2024-07-17 15:17:40 +0900

    feat: 학교검색, 학과검색 API 훅 생성

---
+++ src/pages/onboarding/apis/searchAxios.ts
@@ -0,0 +1,18 @@
+import { axios } from '@utils/apis';
+
+export const univNameAxios = async (univName: string) => {
+  return await axios.get('/v1/search/univ', {
+    params: {
+      univName,
+    },
+  });
+};
+
+export const departmentAxios = async (univName: string, department: string) => {
+  return await axios.get('/v1/search/dept', {
+    params: {
+      univName,
+      department,
+    },
+  });
+};
---
+++ src/pages/onboarding/hooks/useSearchQuery.ts
@@ -0,0 +1,9 @@
+import { useQuery } from '@tanstack/react-query';
+import { univNameAxios } from '../apis/searchAxios';
+
+const useSearchQuery = (univName: string) => {
+  const { data } = useQuery({ queryKey: ['univ'], queryFn: () => univNameAxios(univName) });
+  return data?.data.data.univSearchResult;
+};
+
+export default useSearchQuery;
