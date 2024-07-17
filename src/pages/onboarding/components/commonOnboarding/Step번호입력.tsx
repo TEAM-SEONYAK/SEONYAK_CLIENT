912b6e380c144f56b98e4ed09429cad8863170a9
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-07-18 02:55:19 +0900

    feat: 이미 있는 계정 모달 연결

--- src/pages/onboarding/components/commonOnboarding/Step번호입력.tsx
+++ src/pages/onboarding/components/commonOnboarding/Step번호입력.tsx
@@ -8,6 +8,8 @@ import WarnDescription from '@components/commons/WarnDescription';
 import { AutoCloseModal } from '@components/commons/modal/AutoCloseModal';
 import { useNavigate } from 'react-router-dom';
 import { usePhoneVerify, usePhoneVerifycode } from '@pages/onboarding/hooks/usePhoneQuery';
+import { BtnCloseModal, BtnModalTitle } from '@components/commons/modal/BtnModal';
+import { WarningImg } from '@assets/svgs';
 
 const Step번호입력 = () => {
   const ROLE = 'SENIOR'; // 임시
@@ -21,12 +23,12 @@ const Step번호입력 = () => {
   const verifycodeMutation = usePhoneVerifycode();
   const [isNumError, setIsNumError] = useState(false);
   const [isValidCodeError, setIsValidCodeError] = useState(false);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isDoneModalOpen, setIsDoneModalOpen] = useState(false);
 
   const [phoneNumber, setPhoneNumber] = useState('');
   const [verifyCode, setVerifyCode] = useState('');
   const [isAlreadyPhone, setIsAlreadyPhone] = useState(false);
-  const [isAlreadyModal, setIsAlreadyModalOpen] = useState(false);
+  const [isAlreadyModalOpen, setIsAlreadyModalOpen] = useState(false);
 
   const TIME = 180 * 1000;
   const [timeLeft, setTimeLeft] = useState(TIME);
@@ -50,8 +52,10 @@ const Step번호입력 = () => {
   }, [isActive, timeLeft]);
 
   useEffect(() => {
-    if (verifyCode.length !== 4) return;
-
+    if (verifyCode.length < 4) {
+      setIsValidCodeError(false);
+      return;
+    }
     verifycodeMutation.mutate(
       { phoneNumber: phoneNumber, verificationCode: verifyCode },
       {
@@ -63,6 +67,7 @@ const Step번호입력 = () => {
           if (error.response.data.code === '40902') {
             setIsAlreadyPhone(true);
             setIsNextActive(true);
+            setIsValidCodeError(false);
           } else {
             setIsValidCodeError(true);
           }
@@ -96,14 +101,18 @@ const Step번호입력 = () => {
     setVerifyCode(e.target.value);
   };
 
-  const handleShowModal = (type: boolean) => {
-    setIsModalOpen(type);
+  const handleShowDoneModal = (type: boolean) => {
+    setIsDoneModalOpen(type);
+  };
+
+  const handleShowAlreadyModal = (type: boolean) => {
+    setIsAlreadyModalOpen(type);
   };
 
   const handleClickButton = () => {
     if (isAlreadyPhone) setIsAlreadyModalOpen(true);
     else {
-      setIsModalOpen(true);
+      setIsDoneModalOpen(true);
       setTimeout(() => {
         handleClickLink();
       }, 2000);
@@ -144,9 +153,18 @@ const Step번호입력 = () => {
         )}
       </TextBox>
       <FullBtn text="인증 확인" isActive={timeLeft > 0 && isNextActive} onClick={handleClickButton} />
-      <AutoCloseModal text="인증에 성공했어요" showModal={isModalOpen} handleShowModal={handleShowModal}>
+      <AutoCloseModal text="인증에 성공했어요" showModal={isDoneModalOpen} handleShowModal={handleShowDoneModal}>
         <DummyImage />
       </AutoCloseModal>
+      <BtnCloseModal
+        isModalOpen={isAlreadyModalOpen}
+        handleModalOpen={handleShowAlreadyModal}
+        btnText="로그인 하러 가기"
+        handleBtnClick={() => {
+          navigate('/login');
+        }}>
+        <AlreadyPhoneModalView />
+      </BtnCloseModal>
     </Wrapper>
   );
 };
@@ -171,4 +189,38 @@ const DummyImage = styled.div`
   height: 17rem;
 
   background-color: red;
+`;
+
+const AlreadyPhoneModalView = () => {
+  const PHONEMODAL_TEXT = `이미 서비스에 가입한 번호예요\n변경을 원하시는 경우\n해당 계정으로 로그인을 하신 후\n이의 신청해주시길 바랍니다`;
+  return (
+    <ModalWrapper>
+      <WarningImg />
+      <BtnModalTitle>가입된 계정이 있어요</BtnModalTitle>
+      <ModalDescription>{PHONEMODAL_TEXT}</ModalDescription>
+    </ModalWrapper>
+  );
+};
+
+const ModalWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  align-items: center;
+`;
+
+const ModalDescription = styled.p`
+  display: flex;
+
+  margin-top: 0.5rem;
+  padding: 2rem 3.7rem;
+  border-radius: 9px;
+
+  background-color: ${({ theme }) => theme.colors.grayScaleLG1};
+
+  color: ${({ theme }) => theme.colors.grayScaleDG};
+  text-align: center;
+  white-space: pre-wrap;
+
+  ${({ theme }) => theme.fonts.Body1_M_14};
 `;
