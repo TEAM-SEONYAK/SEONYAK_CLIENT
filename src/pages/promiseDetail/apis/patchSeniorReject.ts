6eaa70276ad66003b9911472dfaf10bda4e5b4c0
se0jinYoon <tjwls1999@gmail.com>
2024-07-18 02:06:58 +0900

    design: 거절 바텀시트 zindex 높임

---
+++ src/pages/promiseDetail/apis/patchSeniorReject.ts
@@ -0,0 +1,8 @@
+// 선배 약속 거절 api
+import { authAxios } from '@utils/apis';
+
+const dummy = {
+  appointmentId: 3,
+  rejectReason: '후배와 시간이 맞지 않아요',
+  rejectDetail: '아주 자세한 이유인데 그냥 싫어요 그냥 싫다구요',
+};
