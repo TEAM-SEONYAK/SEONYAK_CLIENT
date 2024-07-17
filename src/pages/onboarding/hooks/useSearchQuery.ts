403ddb56ac2de187f743609db4f516ba53058a18
lydiacho <sheecho0804@ewhain.net>
2024-07-17 15:36:23 +0900

    feat: 검색기능과 API 연결'

--- src/pages/onboarding/hooks/useSearchQuery.ts
+++ src/pages/onboarding/hooks/useSearchQuery.ts
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import { univNameAxios } from '../apis/searchAxios';
 
 const useSearchQuery = (univName: string) => {
-  const { data } = useQuery({ queryKey: ['univ'], queryFn: () => univNameAxios(univName) });
+  const { data } = useQuery({ queryKey: ['univ', univName], queryFn: () => univNameAxios(univName) });
   return data?.data.data.univSearchResult;
 };
 
