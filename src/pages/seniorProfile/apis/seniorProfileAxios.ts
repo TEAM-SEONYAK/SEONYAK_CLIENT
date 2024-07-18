fd2bfc3e7f7ab1a7d1ac4b8f39beffda88113fe8
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-07-19 03:24:26 +0900

    feat: testAuthAxios 추가

--- src/pages/seniorProfile/apis/seniorProfileAxios.ts
+++ src/pages/seniorProfile/apis/seniorProfileAxios.ts
@@ -1,5 +1,5 @@
 import { seniorProfileAPIType } from "@pages/seniorProfile/types"
-import { authAxios, axios } from "@utils/apis"
+import { authAxios, testAuthAxios } from "@utils/apis"
 
 // export const seniorProfileAxios = ({ catchphrase, career, award, story, preferredTimeList }: seniorProfileAPIType) => {
 //   return authAxios.patch(
@@ -11,15 +11,10 @@ import { authAxios, axios } from "@utils/apis"
 // }
 
 export const seniorProfileAxios = ({ catchphrase, career, award, story, preferredTimeList }: seniorProfileAPIType) => {
-  return axios.patch(
+  return testAuthAxios.patch(
     '/v1/senior/profile',
     {
       catchphrase, career, award, story, preferredTimeList,
     },
-    {
-      headers: {
-        Authorization: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE3MjEyNTY4MzMsImV4cCI6MTc4MTczNjgzMywibWVtYmVySWQiOjQ3fQ.3g0AGA6ZsLBIfSt7_M5ZKTaU1dncXM76M7RI-2TKSOROJHK6fRysM6gIQwymaSFmA0cmtCy9NOryJ-BG4C5wnQ'
-      }
-    }
   )
-}
+};
