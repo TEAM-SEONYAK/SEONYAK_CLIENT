497ca24ab603529f2ae09113ee5da5cf54fe167f
se0jinYoon <tjwls1999@gmail.com>
2024-07-18 20:54:04 +0900

    chore: 코드리뷰 반영

--- src/pages/promiseList/apis/getGoogleMeetLink.ts
+++ src/pages/promiseList/apis/getGoogleMeetLink.ts
@@ -4,7 +4,7 @@ import { authAxios } from '@utils/apis';
 export const getGoogleMeetLink = async (appointmentId: number) => {
   try {
     const response = await authAxios.get(`/v1/google-meet/${appointmentId}`);
-    console.log(response.data.data.googleMeetLink);
+    // console.log(response.data.data.googleMeetLink);
     return response.data.data.googleMeetLink;
   } catch (error) {
     console.error('구글밋 링크 받기 에러:', error);
