cf9494e4a932a2eac05caf506546d48019ac2753
lydiacho <sheecho0804@ewhain.net>
2024-07-18 09:28:36 +0900

    feat: ocrAxios 생성

---
+++ src/pages/onboarding/apis/ocrAxios.ts
@@ -0,0 +1,29 @@
+import { axios } from '@utils/apis';
+
+export const ocrUnivAxios = (imageFile: File) => {
+  return axios.post(
+    '/v1/ocr/univ',
+    {
+      imageFile,
+    },
+    {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    },
+  );
+};
+
+export const ocrNameCardAxios = (imageFile: File) => {
+  return axios.post(
+    '/v1/ocr/business-card',
+    {
+      imageFile,
+    },
+    {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    },
+  );
+};
---
+++ src/pages/onboarding/hooks/useOCRUniv.ts
@@ -0,0 +1,3 @@
+const useOCRUniv = () => {};
+
+export default useOCRUniv;
