d96c12a0d1714bccd70227f212f7394dd4a701a0
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-07-19 05:21:39 +0900

    feat: API request body 타입지정

--- src/pages/onboarding/type.ts
+++ src/pages/onboarding/type.ts
@@ -7,3 +7,20 @@ export interface BizInfoType {
   company: string;
   phoneNumber: string;
 }
+
+
+export interface SignupAPIType {
+  role: 'JUNIOR' | 'SENIOR';
+  isSubscribed: boolean;
+  nickname: string;
+  image: string;
+  phoneNumber: string;
+  univName: string;
+  field: string;
+  departmentList: string[];
+  businessCard?: string;
+  company?: string;
+  position?: string;
+  detailPosition?: string;
+  level?: string;
+}
