b07ec2817d5812d0eae8168ac11920e2e99f009b
Jieun Lee <najee0610@naver.com>
2024-07-20 01:17:20 +0900

    feat: 명함인증 API 연결

--- src/pages/onboarding/components/seniorOnboarding/Step명함인증.tsx
+++ src/pages/onboarding/components/seniorOnboarding/Step명함인증.tsx
@@ -8,9 +8,11 @@ import { ChangeEvent, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { InputBox, TextBox } from '../TextBox';
 import { BizInfoType } from '@pages/onboarding/type';
+import { useBusinessCardQuery } from '@pages/onboarding/hooks/useBusinessCardQuery';
 
 const Step명함인증 = () => {
   const mutation = useOCRBizQuery();
+  const cardmutation = useBusinessCardQuery();
   const navigate = useNavigate();
   const [isOpen, setOpen] = useState(false);
   const [info, setInfo] = useState<BizInfoType | null>(null);
@@ -21,12 +23,27 @@ const Step명함인증 = () => {
     handleSetOpen(true);
   };
 
-  const handleChangeFile = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleChangeFile = async (e: ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files) return;
     const file = e.target.files[0];
+    const token = localStorage.getItem('accessToken');
+
+    if (!token) {
+      console.error('액세스 토큰이 없습니다.');
+      return;
+    }
+
     mutation.mutate(file, {
       onSuccess: (res) => {
         setInfo(res.data.data);
+      },
+    });
+
+    cardmutation.mutate(file, {
+      onSuccess: (res) => {
+      },
+      onError: (error) => {
+        console.error('명함 이미지 업로드 실패:', error);
       },
     });
   };
@@ -151,7 +168,6 @@ const BtnModalBtn = styled.label`
   }
 `;
 
-// VerificationDone
 const DoneWrapper = styled.section`
   display: flex;
   flex-direction: column;
