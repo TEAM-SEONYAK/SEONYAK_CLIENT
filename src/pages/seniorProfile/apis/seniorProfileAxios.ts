62e24b288eaa04393f81a4cd61b22768fa7daaae
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-07-19 20:11:49 +0900

    feat: testAuth 삭제

--- src/pages/seniorProfile/apis/seniorProfileAxios.ts
+++ src/pages/seniorProfile/apis/seniorProfileAxios.ts
@@ -1,17 +1,8 @@
 import { seniorProfileAPIType } from '@pages/seniorProfile/types';
-import { testAuthAxios } from '@utils/apis';
-
-// export const seniorProfileAxios = ({ catchphrase, career, award, story, preferredTimeList }: seniorProfileAPIType) => {
-//   return authAxios.patch(
-//     '/v1/senior/profile',
-//     {
-//       catchphrase, career, award, story, preferredTimeList,
-//     },
-//   )
-// }
+import { authAxios } from '@utils/apis';
 
 export const seniorProfileAxios = ({ catchphrase, career, award, story, preferredTimeList }: seniorProfileAPIType) => {
-  return testAuthAxios.patch('/v1/senior/profile', {
+  return authAxios.patch('/v1/senior/profile', {
     catchphrase,
     career,
     award,
