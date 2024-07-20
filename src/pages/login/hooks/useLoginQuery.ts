fd91bb25074d46771d4dbb82b3e9b6fc9eff9d3a
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-07-20 09:59:34 +0900

    fix: 소셜로그인 12

--- src/pages/login/hooks/useLoginQuery.ts
+++ src/pages/login/hooks/useLoginQuery.ts
@@ -40,7 +40,7 @@ const useGoogleLoginHook = ({ role, variant = 'signup' }: useGoogleLoginPropType
       navigate('/error');
     },
     flow: 'auth-code',
-    redirect_uri: 'https://seonyak.com/login/oauth2/code/google'
+    redirect_uri: 'https://www.seonyak.com/login/oauth2/code/google'
   });
 
   return { login, mutation };
