134843ec9204d4d8168e455472d3a51c698f07ee
se0jinYoon <tjwls1999@gmail.com>
2024-07-20 12:33:51 +0900

    token있는 버전

--- src/utils/apis/index.ts
+++ src/utils/apis/index.ts
@@ -11,6 +11,8 @@ export const authAxios = _axios.default.create({
   baseURL: '/api',
   headers: {
     'Content-Type': 'application/json',
+    Authorization:
+      'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE3MjEzODYyNTgsImV4cCI6MTc4MTg2NjI1OCwibWVtYmVySWQiOjJ9.YTtOIDAgg-qZhuiwA4ZxXeyKBpV6UU1XsSAlxNy1sr_2P1Hl7iL94b0EoR1zxuJfUlcgBH8oY0UZ4kj1VzNGyg',
   },
 });
 
