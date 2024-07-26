fd7c762be613379bb70a4b9ff49008f12293f765
lydiacho <sheecho0804@ewhain.net>
2024-07-26 15:18:12 +0900

    fix: 토큰 하드세팅 제거하고 interceptors로 추가

--- src/utils/apis/index.ts
+++ src/utils/apis/index.ts
@@ -11,20 +11,18 @@ export const authAxios = _axios.default.create({
   baseURL: '/api',
   headers: {
     'Content-Type': 'application/json',
-    Authorization:
-      'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE3MjEzODYyNTgsImV4cCI6MTc4MTg2NjI1OCwibWVtYmVySWQiOjJ9.YTtOIDAgg-qZhuiwA4ZxXeyKBpV6UU1XsSAlxNy1sr_2P1Hl7iL94b0EoR1zxuJfUlcgBH8oY0UZ4kj1VzNGyg',
   },
 });
 
-// authAxios.interceptors.request.use(
-//   (config) => {
-//     const accessToken = localStorage.getItem('accessToken');
-//     if (accessToken) {
-//       config.headers.Authorization = `Bearer ${accessToken}`;
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   },
-// );
+authAxios.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem('accessToken');
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
