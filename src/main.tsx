025b3d2d2e37ad5bc1c54ddd2a0363d8b6703944
lydiacho <sheecho0804@ewhain.net>
2024-08-01 16:57:26 +0900

    remove: 구글로그인 유관 패키지 및 Provider 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,13 @@
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ReactDOM from 'react-dom/client';
 import Router from './Router.tsx';
 
 const queryClient = new QueryClient();
-const clientId = import.meta.env.VITE_APP_GOOGLE_CLIENT_ID + '';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <>
-    <GoogleOAuthProvider clientId={clientId}>
-      <QueryClientProvider client={queryClient}>
-        <Router />
-      </QueryClientProvider>
-    </GoogleOAuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <Router />
+    </QueryClientProvider>
   </>,
 );
