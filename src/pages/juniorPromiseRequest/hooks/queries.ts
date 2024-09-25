d1335ad44a5200b5474365a25ee439c1ff43bab7
featuring-sophia <najee0610@naver.com>
2024-09-25 23:01:15 +0900

    refactor: 약속 신청 에러 핸들링 로직 변경

--- src/pages/juniorPromiseRequest/hooks/queries.ts
+++ src/pages/juniorPromiseRequest/hooks/queries.ts
@@ -1,48 +1,32 @@
 import { useMutation, useQuery } from '@tanstack/react-query';
 
 import { postAppointment } from '../apis/postAppointment';
-import { postAppointmentType } from '../apis/postAppointment';
 import { getSeniorTimeAxios } from '../apis/getSeniorTimeAxios';
 import axios from 'axios';
 
-export const QUERY_KEY_JUNIOR_PROMISE_SEND = {
-  postAppointment: postAppointment,
-  getSeniorTimeAxios: getSeniorTimeAxios,
+const QUERY_KEYS = {
+  postAppointment: 'postAppointment',
+  getSeniorTime: 'getSeniorTime',
 };
 
-export const usePostAppointment = (onSuccessCallback?: () => void, onErrorCallback?: (error: string) => void) => {
-  const { mutate, data } = useMutation({
-    mutationKey: [QUERY_KEY_JUNIOR_PROMISE_SEND.postAppointment],
-    mutationFn: ({ seniorId, topic, personalTopic, timeList }: postAppointmentType) =>
-      postAppointment({ seniorId, topic, personalTopic, timeList }),
-    onSuccess: () => {
-      if (onSuccessCallback) {
-        onSuccessCallback();
-      }
-    },
+export const usePostAppointment = (onSuccess?: () => void, onError?: (error: string) => void) => {
+  return useMutation({
+    mutationKey: [QUERY_KEYS.postAppointment],
+    mutationFn: postAppointment,
+    onSuccess,
     onError: (error) => {
-      let errorMessage = '알 수 없는 오류가 발생했습니다.';
-      if (axios.isAxiosError(error) && error.response) {
-        if (error.response?.status === 403 || error.response?.status === 400) {
-          errorMessage = '이미 약속을 신청한 선배입니다.';
-        } else {
-          errorMessage = '약속 신청에 실패했습니다.';
-        }
-      }
-      if (onErrorCallback) {
-        onErrorCallback(errorMessage);
-      }
+      const errorMessage =
+        axios.isAxiosError(error) && error.response?.data?.message
+          ? error.response.data.message
+          : '알 수 없는 오류가 발생했습니다.';
+      onError?.(errorMessage);
     },
   });
-
-  return { mutate, data };
 };
 
 export const useSeniorTimeQuery = (seniorId: number) => {
-  const { data, isSuccess, isLoading, isError } = useQuery({
-    queryKey: [QUERY_KEY_JUNIOR_PROMISE_SEND.getSeniorTimeAxios, seniorId],
+  return useQuery({
+    queryKey: [QUERY_KEYS.getSeniorTime, seniorId],
     queryFn: () => getSeniorTimeAxios(seniorId),
   });
-
-  return { data, isSuccess, isLoading, isError };
 };
