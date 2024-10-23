c05a2ce9759ec09cb21274f9198a0e93f8097ae9
se0jinYoon <tjwls1999@gmail.com>
2024-10-24 04:14:52 +0900

    chore: suspense 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ReactDOM from 'react-dom/client';
 import Router from './Router.tsx';
+import { Suspense } from 'react';
+import Loading from '@components/commons/Loading.tsx';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <>
     <QueryClientProvider client={queryClient}>
-      <Router />
+      <Suspense fallback={<Loading />}>
+        <Router />
+      </Suspense>
     </QueryClientProvider>
   </>
 );
