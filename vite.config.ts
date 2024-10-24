ac69cd95714d7f4efb6a2e7e8ffa43c4429549a8
se0jinYoon <tjwls1999@gmail.com>
2024-10-24 20:52:52 +0900

    chore: Suspense없이, 에러페이지 분리 테스트

--- vite.config.ts
+++ vite.config.ts
@@ -29,5 +29,17 @@ export default defineConfig(({ mode }) => {
         },
       },
     },
+    // build: {
+    //   chunkSizeWarningLimit: 1600,
+    //   rollupOptions: {
+    //     output: {
+    //       manualChunks(id) {
+    //         if (id.includes('node_modules')) {
+    //           return id.toString().split('node_modules/')[1].split('/')[0].toString();
+    //         }
+    //       },
+    //     },
+    //   },
+    // },
   };
 });
