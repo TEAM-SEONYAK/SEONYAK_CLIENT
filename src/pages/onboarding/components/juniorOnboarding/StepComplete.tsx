e05cadf21f6dc24bce21b637aa843a5bd70fb0c6
lydiacho <sheecho0804@ewhain.net>
2024-10-29 17:34:05 +0900

    fix: 완료 뷰 GUI 수정 사항 반영

--- src/pages/onboarding/components/juniorOnboarding/StepComplete.tsx
+++ src/pages/onboarding/components/juniorOnboarding/StepComplete.tsx
@@ -1,17 +1,13 @@
 import { ProfileCompleteImg } from '@assets/images';
 import styled from '@emotion/styled';
-import { JoinContextType } from '@pages/onboarding/type';
-import { useOutletContext } from 'react-router-dom';
 
 const StepComplete = () => {
-  const { data } = useOutletContext<JoinContextType>();
-
   return (
     <Wrapper>
       <Title>
-        {data.nickname} 후배님
+        고생하셨어요!
         <br />
-        반가워요!
+        회원가입이 완료되었어요
       </Title>
       <Description>바로 선약을 시작하고, 선배와 약속을 잡아보세요</Description>
       <Img src={ProfileCompleteImg} alt="" />
